26674b9105c4ca3ad818527a9c2c21ba22176e68
blysca <blysca@gmail.com>
2020-12-13 11:09:12 +0200

    finished table resize

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,10 +1,28 @@
 import { ExcelComponont } from '@core/ExcelComponont';
 import { createTable } from '@/components/table/table.template';
+import { resizeHandler } from '@/components/table/table.resize';
+import { shouldResize } from '@/components/table/table.functions';
 
 export class Table extends ExcelComponont {
   static className = 'excel__table'
 
+  resizing = false
+  prevX = 0
+  prevY = 0
+
+  constructor( $root ) {
+    super( $root, {
+      listeners: [ 'mousedown' ]
+    } )
+  }
+
   toHTML() {
     return createTable()
+  }
+
+  onMousedown( event ) {
+    if ( shouldResize( event ) ) {
+      resizeHandler( this.$root, event )
+    }
   }
 }
---
+++ src/components/table/table.resize.js
@@ -0,0 +1,48 @@
+import { $ } from '@core/dom';
+
+export function resizeHandler($root, event) {
+  const direction = event.target.dataset.resize
+
+  const $resizeEl = $( event.target )
+  const $parent = $resizeEl.closest( '[data-type="resizeble" ]' )
+  const coordinate = $parent.getCoords()
+  const columnIndex = $parent.data.col
+
+  const sideProp = ( direction === 'col' ? 'bottom' : 'right' )
+  const sidePropValue = ( direction === 'col' ? '-100vh' : '-100vw' )
+
+  let value
+  $resizeEl.css( {
+    opacity: 1,
+    [ sideProp ]: sidePropValue
+  } )
+
+  document.onmousemove = ev => {
+    if ( direction === 'col' ) {
+      const difference = Math.floor( ev.pageX - coordinate.right )
+      value = coordinate.width + difference
+      $resizeEl.css( { right: -difference + 'px' } )
+    } else {
+      const difference = Math.floor( ev.pageY - coordinate.bottom )
+      $resizeEl.css( { bottom: -difference + 'px' } )
+      value = coordinate.height + difference
+    }
+  }
+
+  document.onmouseup = () => {
+    document.onmousemove = null
+    document.onmouseup = null
+
+    if ( direction === 'col' ) {
+      $parent.css( { width: `${ value }px` } )
+      $root
+          .findAll( `[data-col="${ columnIndex }"]` )
+          .forEach( element => element.style.width = `${ value }px` )
+    } else if ( direction === 'row' ) {
+      $parent.css( { height: `${ value }px` } )
+    }
+
+    $resizeEl.$el.classList.remove( 'active' )
+    $resizeEl.$el.removeAttribute( 'style' )
+  }
+}
